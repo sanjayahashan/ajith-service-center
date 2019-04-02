cacea8891d226bf48e6f5392ff0cc5de54625d96
SanJay <sanjayahashan@live.com>
2019-04-02 15:31:41 +0530

    registration frontend updated

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -74,15 +74,45 @@
                     <!-- Login Form -->
                     <div class="login-form">
                         <!--Login Form-->
-                        <form method="post" action="http://expert-themes.com/html/motor-expert/login.html">
+                        <form method="post" action="{{route('register')}}">
+                        @csrf
+
+                            <div class="form-group">
+                                <label for="name">{{ __('Name') }}</label>
+                                    <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
+
+                                    @if ($errors->has('name'))
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $errors->first('name') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+
                             <div class="form-group">
                                 <label>Email Address</label>
-                                <input type="text" name="username" placeholder="Your Email " required>
+                                <input type="text" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" placeholder="Your Email " required>
+
+                                @if ($errors->has('email'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
                             </div>
                             
                             <div class="form-group">
                                 <label>Enter Your Password</label>
-                                <input type="password" name="password" placeholder="Password" required>
+                                <input type="password"  class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" placeholder="Password" required>
+                                
+                                @if ($errors->has('password'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+
+                            <div class="form-group">
+                                <label for="password-confirm">Confirm Password</label>
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
                             </div>
                             
                             <div class="clearfix">
